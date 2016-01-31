5dadee9cca50827e430dce14e8adb582bd7d613d
Joze Kulovic <joze.kulovic@xlab.si>
2016-01-31 10:47:36 +0100

    implementing custom metronome light view

---
+++ app/src/main/java/com/jst10/by/made/metronome/custom/views/Light.java
@@ -0,0 +1,113 @@
+package com.jst10.by.made.metronome.custom.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.jst10.by.made.metronome.R;
+
+/**
+ * Created by jst10 on 31.1.2016.
+ */
+public class Light extends View {
+    private static final int TYPE_MAIN = 0;
+    private static final int TYPE_MINOR = 1;
+
+    private int type;
+    private int borderColor;
+    private int activatedColor;
+    private int deactivatedColor;
+
+    private Paint activatedPaint;
+    private Paint deactivatedPaint;
+
+    public Light(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public Light(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attributeSet) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(attributeSet, R.styleable.Light, 0, 0);
+        type = TYPE_MAIN;
+        try {
+            type = a.getInteger(R.styleable.Light_type, TYPE_MAIN);
+        } finally {
+            initColors();
+            a.recycle();
+        }
+    }
+
+    private void initColors() {
+        if (type == TYPE_MAIN) {
+            this.borderColor = getResources().getColor(R.color.light_border_1);
+            this.deactivatedColor = getResources().getColor(R.color.light_deactivated_1);
+            this.activatedColor = getResources().getColor(R.color.light_activated_1);
+        } else if (type == TYPE_MINOR) {
+            this.borderColor = getResources().getColor(R.color.light_border_2);
+            this.deactivatedColor = getResources().getColor(R.color.light_deactivated_2);
+            this.activatedColor = getResources().getColor(R.color.light_activated_2);
+        } else {
+            throw new IllegalAccessError();
+        }
+
+        if (activatedPaint == null || deactivatedPaint == null) {
+            activatedPaint = new Paint();
+            activatedPaint.setStyle(Paint.Style.FILL);
+            activatedPaint.setAntiAlias(true);
+            activatedPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+
+            deactivatedPaint = new Paint();
+            deactivatedPaint.setStyle(Paint.Style.FILL);
+            deactivatedPaint.setAntiAlias(true);
+            deactivatedPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+
+        }
+
+        activatedPaint.setColor(this.activatedColor);
+        deactivatedPaint.setColor(this.deactivatedColor);
+
+
+    }
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint;
+        if (isActivated()) {
+            paint = activatedPaint;
+        } else {
+            paint = deactivatedPaint;
+        }
+
+        int x = getWidth();
+        int y = getHeight();
+        canvas.drawCircle(x/2, y/2, x/2, paint);
+    }
+
+    public void setType(int type) {
+        this.type = type;
+        initColors();
+    }
+
+    public void setBorderColor(int borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    public void setActivatedColor(int activatedColor) {
+        this.activatedColor = activatedColor;
+    }
+
+    public void setDeactivatedColor(int deactivatedColor) {
+        this.deactivatedColor = deactivatedColor;
+    }
+}
--- app/src/main/java/com/jst10/by/made/metronome/fragments/TopFragment.java
+++ app/src/main/java/com/jst10/by/made/metronome/fragments/TopFragment.java
@@ -1,9 +1,42 @@
 package com.jst10.by.made.metronome.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jst10.by.made.metronome.R;
+import com.jst10.by.made.metronome.custom.views.Light;
 
 /**
  * Created by jst10 on 17.1.2016.
  */
 public class TopFragment extends Fragment {
+        private View layoutView;
+
+        private Light[] lights;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            layoutView = inflater.inflate(R.layout.top_fragment, container, false);
+            bindViews();
+            init();
+            return layoutView;
+        }
+
+        private void bindViews() {
+            lights=new Light[4];
+            lights[0]= (Light) layoutView.findViewById(R.id.light1);
+            lights[1]= (Light) layoutView.findViewById(R.id.light2);
+            lights[2]= (Light) layoutView.findViewById(R.id.light3);
+            lights[3]= (Light) layoutView.findViewById(R.id.light4);
+
+        }
+
+        private void init() {
+
+            lights[1].setActivated(true);
+            lights[3].setActivated(true);
+        }
+
 }
